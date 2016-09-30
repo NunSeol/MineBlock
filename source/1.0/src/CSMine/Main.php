456cc75632866e68dc1b2ac68332b7c23020f969
angelless <dongkyu91622@gmail.com>
2016-10-01 02:56:14 +0900

    Code Fix

--- source/1.0/src/CSMine/Main.php
+++ source/1.0/src/CSMine/Main.php
@@ -20,260 +20,45 @@ class Main extends PluginBase implements Listener {
 	public function BreakBlock(BlockBreakEvent $event) {
 		$player = $event->getPlayer();
 		if ($event->getBlock()->getId() == 14) {
-			switch (mt_rand(0, 50)) {
-				case "0":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "1":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "3":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "4":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "5":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "6":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "7":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "8":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "9":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "10":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "11":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "12":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "13":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "14":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "15":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
-				case "16":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "17":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "18":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "19":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "20":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "21":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "22":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "23":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "24":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "25":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "26":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 10);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
-				case "27":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "28":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "29":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "30":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "31":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "32":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "33":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "34":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "35":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "36":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "37":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 50);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
-				case "38":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 100);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("100원이 지급되었습니다!");
-				case "39":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 100);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("100원이 지급되었습니다!");
-				case "40":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 100);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("100원이 지급되었습니다!");
-				case "41":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 100);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("100원이 지급되었습니다!");
-				case "42":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 100);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("100원이 지급되었습니다!");
-				case "43":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 500);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("500원이 지급되었습니다!");
-				case "44":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 500);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("500원이 지급되었습니다!");
-				case "45":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 500);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("500원이 지급되었습니다!");
-				case "46":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 500);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("500원이 지급되었습니다!");
-				case "47":
-					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 500);
-					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("500원이 지급되었습니다!");
-				case "48":
+			$num = mt_rand(0, 50);
+                            if($num>48){
+
 					$this->getServer()->getLevel()->setBlock($event, 14);
 					EconomyAPI::getInstance()->addMoney($player, 1000);
 					$player->removeCreativeItem(14);
 					$event->getPlayer()->sendMessage("1000원이 지급되었습니다!");
-				case "49":
+
+                                }else if($num >43){
+                                  
 					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1000);
+					EconomyAPI::getInstance()->addMoney($player, 500);
 					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1000원이 지급되었습니다!");
-				case "50":
+					$event->getPlayer()->sendMessage("500원이 지급되었습니다!");
+                                }else if($num>38){
+
 					$this->getServer()->getLevel()->setBlock($event, 14);
-					EconomyAPI::getInstance()->addMoney($player, 1000);
+					EconomyAPI::getInstance()->addMoney($player, 100);
 					$player->removeCreativeItem(14);
-					$event->getPlayer()->sendMessage("1000원이 지급되었습니다!");
-				return;
-				break;
-			}
+					$event->getPlayer()->sendMessage("100원이 지급되었습니다!");
+
+                                }else if ($num > 27){
+					$this->getServer()->getLevel()->setBlock($event, 14);
+					EconomyAPI::getInstance()->addMoney($player, 50);
+					$player->removeCreativeItem(14);
+					$event->getPlayer()->sendMessage("50원이 지급되었습니다!");
+                                }else if ($num > 16){
+                                        $this->getServer()->getLevel()->setBlock($event, 14);
+					EconomyAPI::getInstance()->addMoney($player, 10);
+					$player->removeCreativeItem(14);
+					$event->getPlayer()->sendMessage("10원이 지급되었습니다!");
+                                }else{
+                                        $this->getServer()->getLevel()->setBlock($event, 14);
+					EconomyAPI::getInstance()->addMoney($player, 1);
+					$player->removeCreativeItem(14);
+					$event->getPlayer()->sendMessage("1원이 지급되었습니다!");
+			
+                                }
+return;
 		}
 	}
 	public function onDisable() {
